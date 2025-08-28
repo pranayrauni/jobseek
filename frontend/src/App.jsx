ad16d5a39e68f182ecf620b7b96ce33ef7e3b2dd
pranayrauni <ipranaysingh@gmail.com>
2025-08-28 06:45:07 +0530

    signup login navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/shared/Navbar'
+import Signup from './components/auth/Signup'
+import Login from './components/auth/Login'
+import Home from './components/Home'
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/signup',
+    element: <Signup/>
+  }
+])
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <RouterProvider router={appRouter} />
+    
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/auth/Signup.jsx
@@ -0,0 +1,164 @@
+import React, { useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { USER_API_END_POINT } from "@/utils/constant";
+import { toast } from "sonner";
+
+const Signup = () => {
+  const [input, setInput] = useState({
+    fullname: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    role: "",
+    file: "",
+  });
+
+  const navigate = useNavigate();
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const changeFileHandler = (e) => {
+    setInput({ ...input, file: e.target.files?.[0] });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("fullname", input.fullname);
+    formData.append("email", input.email);
+    formData.append("phoneNumber", input.phoneNumber);
+    formData.append("password", input.password);
+    formData.append("role", input.role);
+
+    if(input.file){
+        formData.append("file", input.file);
+    }
+
+    try {
+        const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
+            headers: {
+                "Content-Type": "multipart/form-data"
+            },
+            withCredentials: true,
+        })
+
+        if(res.data.success){
+            navigate("/login")
+            toast.success(res.data.message);
+        }
+    } catch (error) {
+        console.log(error)
+        toast.error(error.response?.data?.message || "Something went wrong")
+    }
+  };
+  return (
+    <div>
+      <Navbar />
+
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          action=""
+          onSubmit={submitHandler}
+          className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
+        >
+          <h1 className="font-bold text-xl mb-5">Sign Up</h1>
+
+          <div className="my-2">
+            <Label>Full Name</Label>
+            <Input
+              type="text"
+              placeholder="Enter Full Name"
+              value={input.fullname}
+              name="fullname"
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input
+              type="email"
+              placeholder="Enter Email"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="my-2">
+            <Label>Phone Number</Label>
+            <Input
+              type="text"
+              placeholder="Enter Phone Number"
+              value={input.phoneNumber}
+              name="phoneNumber"
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input
+              type="password"
+              placeholder="Enter Password"
+              value={input.password}
+              name="password"
+              onChange={changeEventHandler}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <RadioGroup className="flex items-center gap-4 my-5">
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="candidate"
+                  checked={input.role === "candidate"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r1">Candidate</Label>
+              </div>
+              <div className="flex items-center gap-3">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  checked={input.role === "recruiter"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup>
+            <div className="flex items-center gap-2">
+              <Label>Profile</Label>
+              <Input
+                accept="image/*"
+                type="file"
+                className="cursor-pointer"
+                onChange={changeFileHandler}
+              />
+            </div>
+          </div>
+          <Button type="submit" className="w-full my-4">
+            Signup
+          </Button>
+          <span className="text-sm">
+            Already have an account?{" "}
+            <Link to="/login" className="text-blue-600">
+              Login
+            </Link>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
