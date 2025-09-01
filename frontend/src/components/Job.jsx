04bb1ca9ca5c4e336e908d29059f679751fe5900
pranayrauni <ipranaysingh@gmail.com>
2025-09-01 23:48:25 +0530

    job description

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -3,8 +3,13 @@ import { Button } from "./ui/button";
 import { Bookmark } from "lucide-react";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
+import { useNavigate } from "react-router-dom";
 
 const Job = () => {
+
+  const navigate = useNavigate();
+  const jobId = "jkkd"
+
   return (
     <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100">
       <div className="flex items-center justify-between">
@@ -48,7 +53,7 @@ const Job = () => {
       </div>
 
       <div className="flex items-center gap-4 mt-4">
-        <Button variant="outline">Details</Button>
+        <Button onClick={() => navigate(`/description/${jobId}`)} variant="outline">Details</Button>
         <Button className="bg-[#7209b7]">Save for later</Button>
       </div>
     </div>
