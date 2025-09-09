ba7e38fc50a8739ca5ae3c1742cea88d0313a97e
pranayrauni <ipranaysingh@gmail.com>
2025-09-09 22:47:29 +0530

    filter company

--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -14,7 +14,23 @@ import { Edit2, MoreHorizontal } from "lucide-react";
 import { useSelector } from "react-redux";
 
 const CompaniesTable = () => {
-  const { companies } = useSelector((store) => store.company);
+  const { companies, searchCompanyByText } = useSelector((store) => store.company);
+
+  const [filterCompany, setFilterCompany] = useState(companies);
+
+  useEffect(()=> {
+    const filteredCompany = companies.length >= 0 && companies.filter((company) => {
+      if(!searchCompanyByText){
+        return true;
+      }
+
+      return company?.name?.toLowerCase().includes(searchCompanyByText.toLowerCase());
+      
+    })
+
+    setFilterCompany(filteredCompany);
+
+  }, [companies, searchCompanyByText])
 
   return (
     <div>
@@ -36,7 +52,7 @@ const CompaniesTable = () => {
               </TableCell>
             </TableRow>
           ) : (
-            companies.map((company) => (
+            filterCompany?.map((company) => (
               <TableRow key={company._id}>
                 <TableCell>
                   <Avatar>
