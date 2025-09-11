766cb04150c85303114915870c1055ad57e0580d
pranayrauni <ipranaysingh@gmail.com>
2025-09-11 23:34:53 +0530

    cors update

--- backend/index.js
+++ backend/index.js
@@ -23,20 +23,21 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: '*',
+    origin: process.env.CLIENT_URL,
     credentials: true
 }
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
+connectDB();
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+
 app.listen(PORT, () => {
-    connectDB();
     console.log(`Server running at port ${PORT}`); 
 })
